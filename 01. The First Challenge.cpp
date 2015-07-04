40e226cfbe27470bf135d4fb7a4b90cbb2c73e3c
dipuhasan <depu1994@gmail.com>
2015-07-04 09:28:00 +0600

    Create 01. The First Challenge.cpp

---
+++ 01. The First Challenge.cpp
@@ -0,0 +1,21 @@
+
+/*
+ID: depu1991
+PROG: test
+LANG: C++
+*/
+
+#include<cstdio>
+using namespace std;
+main ()
+{
+    int a, b;
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w",stdout);
+    scanf("%d %d", &a, &b);
+
+    printf("%d\n", a+b);
+
+    return 0;
+
+}
